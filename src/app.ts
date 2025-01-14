431898474837b6a2221f9265580041ef59e18996
YosatoW <wy.onacc@hotmail.com>
2025-01-14 09:02:00 +0100

    feat: set up Nuxt 3 frontend with Docker support, add basic layout and components

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, { Router, type Request, type Response} from 'express'
 import { initializeAPI } from './api'
-
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 initializeAPI(app)
 
