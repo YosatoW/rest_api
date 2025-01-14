55ee827ec54936af07ef0e60c6c3c378c4c892b0
YosatoW <wy.onacc@hotmail.com>
2025-01-14 13:22:43 +0100

    refactor: optimize close.bat script by commenting out warnings and reducing timeout duration

--- close.bat
+++ close.bat
@@ -1,11 +1,11 @@
 @echo off
-echo Warning: This will close all CMD windows
-echo Press Ctrl+C to cancel or...
-pause
+@REM echo Warning: This will close all CMD windows
+@REM echo Press Ctrl+C to cancel or...
+@REM pause
 
 REM Start Docker
 docker compose down
-timeout /t 2
+timeout /t 1
 
 echo Closing specific windows...
 taskkill /F /FI "WINDOWTITLE eq Backend*" /T
@@ -13,4 +13,4 @@ taskkill /F /FI "WINDOWTITLE eq Drizzle*" /T
 taskkill /F /FI "WINDOWTITLE eq Frontend*" /T
 
 echo Done!
-timeout /t 2
+timeout /t 1
